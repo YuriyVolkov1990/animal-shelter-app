520f33a02e321de57a2a5d9479f5445727e2f2a3
Professional <new99kpicem@gmail.com>
2023-10-08 18:08:16 +0300

    feature_work

--- src/main/java/com/animalshelter/animalshelterapp/entity/Shelter.java
+++ src/main/java/com/animalshelter/animalshelterapp/entity/Shelter.java
@@ -1,4 +1,12 @@
 package com.animalshelter.animalshelterapp.entity;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name = "shelter")
 public class Shelter {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 }
