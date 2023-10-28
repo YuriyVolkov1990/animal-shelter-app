e0ef1582bc4f479a2ce862f65c0a999a138a2045
Professional <new99kpicem@gmail.com>
2023-10-28 16:32:51 +0300

    feature_work

---
+++ src/main/java/com/animalshelter/animalshelterapp/service/ShelteringService.java
@@ -0,0 +1,4 @@
+package com.animalshelter.animalshelterapp.service;
+
+public interface ShelteringService {
+}
---
+++ src/main/java/com/animalshelter/animalshelterapp/service/impl/CatShelteringService.java
@@ -0,0 +1,6 @@
+package com.animalshelter.animalshelterapp.service.impl;
+
+import com.animalshelter.animalshelterapp.service.ShelteringService;
+
+public class CatShelteringService implements ShelteringService {
+}
